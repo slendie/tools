cea5062d61ad7ce012bf9f70b44fc266793dd06c
lcloss <lcloss@gmail.com>
2022-05-26 22:57:37 +0100

    Internal links

--- src/Url.php
+++ src/Url.php
@@ -42,4 +42,61 @@ class Url
 
         return $matches[1];
     }
+
+    public static function isInternalLink( $domain_url, $page_url ) 
+    {
+        if ( Str::startsWith( 'http', $page_url ) ) {
+            return Str::startsWith( $domain_url, $page_url );
+        }
+        if ( Str::startsWith( '/', $page_url ) && !startsWith( '//', $page_url ) ) {
+            if ( Str::endsWith( '/', $domain_url ) ) {
+                $domain = substr( $domain_url, -1 );
+            } else {
+                $domain = $domain_url;
+            }
+            $url = $domain_url . $page_url;
+            return true;
+        }
+        if ( Str::startsWith( '//', $page_url ) ) {
+            $pageUrl = Url( $domain_url );
+            $protocol = $pageUrl->getProtocol();
+
+            $url = $protocol . ':' . $page_url;
+
+            return Str::startsWith( $domain_url, $url );
+        }
+        return false;
+    }
+
+    public static function getInternalLink( $domain_url, $page_url ) 
+    {
+        if ( Str::startsWith( 'http', $page_url ) ) {
+            if ( Str::startsWith( $domain_url, $page_url ) ) {
+                return $page_url;
+            }
+            return false;
+        }
+        if ( Str::startsWith( '/', $page_url ) && !startsWith( '//', $page_url ) ) {
+            if ( Str::endsWith( '/', $domain_url ) ) {
+                $domain = substr( $domain_url, -1 );
+            } else {
+                $domain = $domain_url;
+            }
+            $url = $domain_url . $page_url;
+            return $url;
+        }
+        if ( Str::startsWith( '//', $page_url ) ) {
+            $pageUrl = Url( $domain_url );
+            $protocol = $pageUrl->getProtocol();
+
+            $url = $protocol . ':' . $page_url;
+
+            if ( Str::startsWith( $domain_url, $url ) ) {
+                return $url;
+            }
+            return false;
+        }
+        return false;
+    }
+
 }
