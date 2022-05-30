a2934819f350729ea52fc94efd2197c1a6d482d4
lcloss <lcloss@gmail.com>
2022-05-31 00:47:06 +0100

    Refactoring

--- src/Str.php
+++ src/Str.php
@@ -128,4 +128,51 @@ class Str
 
         return $term . "s";
     }
+
+    public static function singular( $term )
+    {
+        $test = strtolower( $term );
+
+        $word_exceptions = [
+            'children'     => 
+                [   
+                    'singular'        => 'child',
+                    'operations'    => [
+                        '-|3',
+                    ],
+                ],
+            'people'    => 
+                [
+                    'singular'        => 'person',
+                    'operations'    => [
+                        '-|4',
+                        '+|rson',
+                    ],
+                ],
+        ];
+
+        // Ending with 'ES'
+        $word_group_1_exceptions = [
+            '*'       =>
+                [
+                    'singular'        => '*',
+                    'operations'    => [
+                        '-|2',
+                    ],
+                ],
+    ];
+
+        // Check exceptions
+        $finding = self::processWords( $term, $word_exceptions );
+        if ( $finding !== false ) return $finding;
+
+        // Ending with special characters - group 1
+        if ( self::endsWith( 'es', $test ) ) {
+            $finding = self::processWords( $term, $word_group_1_exceptions );
+            if ( $finding !== false ) return $finding;
+        }
+
+        return substr( $term, 0, -1 );
+    }
+
 }
