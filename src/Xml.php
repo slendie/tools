a714d8c0b42a38badb11d01ec8f199df3d5457c9
lcloss <lcloss@gmail.com>
2022-06-25 13:44:09 +0100

    XML

---
+++ src/Xml.php
@@ -0,0 +1,35 @@
+<?php
+namespace Slendie\Tools;
+
+class Xml
+{
+    public static function arrayToXml( $array )
+    {
+        $xml = new SimpleXmlElement('<?xml version="1.0" encoding="utf-8"?><root></root>');
+
+        self::addArrayToXml( $array, $xml );
+
+        return $xml->asXML();
+    }
+
+    private static function addArrayToXml( $array, $xml )
+    {
+        if ( is_array( $array ) ) {
+            foreach( $array as $key => $value ) {
+                if ( is_int( $key ) ) {
+                    if ( $key == 0 ) {
+                        $node = $xml;
+                    } else {
+                        $parent = $xml->xpath('..')[0];
+                        $node = $parent->addChild( $xml->getName() );
+                    }
+                } else {
+                    $node = $xml->addChild( $key );
+                }
+                self::addArrayToXml( $value, $node );
+            }
+        } else {
+            $xml[0] = $array;
+        }
+    }
+}
